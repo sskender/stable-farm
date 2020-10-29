4fa5e880a202937b3d8a3586c9719a34ea2e0c20
sskender <sven.skender@fer.hr>
2020-10-29 16:33:26 +0100

    token test - tests broken to smaller peaces

--- ethereum/test/daotoken.test.js
+++ ethereum/test/daotoken.test.js
@@ -1,16 +1,28 @@
 const DaoToken = artifacts.require('DaoToken');
 
 contract('DaoToken test', async (accounts) => {
-  it('it should mint one token to deployer', async () => {
+  it('it should set default token values', async () => {
     const instance = await DaoToken.deployed();
+    const name = await instance.name();
     const symbol = await instance.symbol();
-    const supply = await instance.totalSupply();
-    const balance = await instance.balanceOf(accounts[0]);
-    const holders = await instance.tokenHolders();
+    const granularity = await instance.granularity();
+    const mintable = await instance.isMintable();
+    assert.equal(name, 'EESTEC LC Zagreb');
     assert.equal(symbol, 'EZG');
+    assert.equal(granularity.valueOf(), 1);
+    assert.equal(mintable, false);
+  });
+
+  it('it should mint one token to deployer', async () => {
+    const deployerAcc = accounts[0];
+    const instance = await DaoToken.deployed();
+    const supply = await instance.totalSupply();
+    const balance = await instance.balanceOf(deployerAcc);
+    const holders = await instance.tokenHolders();
     assert.equal(supply.valueOf(), 1);
     assert.equal(balance.valueOf(), 1);
     assert.equal(holders.length, 1);
+    assert.equal(holders[0], deployerAcc);
   });
 
   it('it should enable and disable minting', async () => {
