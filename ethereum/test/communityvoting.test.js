2a9624577ee2c10324890581f2c18b9146a8091b
sskender <sven.skender@fer.hr>
2020-11-01 02:26:02 +0100

    community voting test - creating proposition

--- ethereum/test/communityvoting.test.js
+++ ethereum/test/communityvoting.test.js
@@ -1,13 +1,175 @@
+/* eslint-disable no-undef */
+const DaoToken = artifacts.require('DaoToken');
 const CommunityVoting = artifacts.require('CommunityVoting');
 
 contract('CommunityVoting test', async (accounts) => {
-  it('it should set default token values', async () => {
+  it('it should set default contract values', async () => {
     const instance = await CommunityVoting.deployed();
     const name = await instance.getName();
     const propositions = await instance.getNumberOfPropositions();
-    const tokenAddress = await instance.getTokenAddress();
     assert.equal(name, 'EESTEC LC Zagreb Basic Voting Contract');
     assert.equal(propositions.valueOf(), 0);
+  });
+
+  it('it should set default token values', async () => {
+    const instance = await CommunityVoting.deployed();
+    const tokenInstance = await DaoToken.deployed();
+    const tokenAddress = await instance.getTokenAddress();
     assert.equal(web3.utils.isAddress(tokenAddress), true);
+    assert.equal(tokenInstance.address, tokenAddress);
+  });
+
+  it('it should prevent proposition with invalid start block 1', async () => {
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    let propositionId = null;
+    let e = null;
+    try {
+      propositionId = await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber - 1,
+        blockNumber + 15
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'Invalid start block number');
+    assert.equal(propositionId, null);
+  });
+
+  it('it should prevent proposition with invalid start block 2', async () => {
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    let propositionId = null;
+    let e = null;
+    try {
+      propositionId = await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber,
+        blockNumber + 15
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'Invalid start block number');
+    assert.equal(propositionId, null);
+  });
+
+  it('it should prevent proposition with invalid end block 1', async () => {
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    let propositionId = null;
+    let e = null;
+    try {
+      propositionId = await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber + 1,
+        blockNumber
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'Invalid end block number');
+    assert.equal(propositionId, null);
+  });
+
+  it('it should prevent proposition with invalid end block 2', async () => {
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    let propositionId = null;
+    let e = null;
+    try {
+      propositionId = await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber + 1,
+        blockNumber + 1
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'Invalid end block number');
+    assert.equal(propositionId, null);
+  });
+
+  it('it should prevent proposition with invalid end block 3', async () => {
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    let propositionId = null;
+    let e = null;
+    try {
+      propositionId = await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber + 1,
+        blockNumber + 10
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'Invalid end block number');
+    assert.equal(propositionId, null);
+  });
+
+  it('it should fail to create proposition from non member', async () => {
+    const nonMember = accounts[5];
+    const blockNumber = await web3.eth.getBlockNumber();
+    const instance = await CommunityVoting.deployed();
+    const totalPropositions = await instance.getNumberOfPropositions();
+    let e = null;
+    try {
+      await instance.createProposition(
+        'Prop title',
+        'Prop desc',
+        blockNumber + 2,
+        blockNumber + 11,
+        {from: nonMember}
+      );
+    } catch (err) {
+      e = err.reason;
+    }
+    assert.equal(e, 'You are not an active member');
+    assert.equal(totalPropositions.valueOf(), 0);
+  });
+
+  it('it should fail to fetch non-existing proposition', async () => {
+    const instance = await CommunityVoting.deployed();
+    const totalPropositions = await instance.getNumberOfPropositions();
+    let e = null;
+    try {
+      await instance.getPropositionInfo(0);
+    } catch (err) {
+      e = JSON.stringify(err);
+    }
+    assert.equal(e.includes('Invalid proposition Id'), true);
+    assert.equal(totalPropositions.valueOf(), 0);
+  });
+
+  it('it should create a new proposition from deployer', async () => {
+    const deployer = accounts[0];
+    const instance = await CommunityVoting.deployed();
+    const blockNumber = await web3.eth.getBlockNumber();
+    await instance.createProposition(
+      'Prop title',
+      'Prop desc',
+      blockNumber + 2,
+      blockNumber + 11
+    );
+    const totalPropositions = await instance.getNumberOfPropositions();
+    const propositionInfo = await instance.getPropositionInfo(0);
+
+    assert.equal(totalPropositions.valueOf(), 1);
+    assert.equal(propositionInfo.title, 'Prop title');
+    assert.equal(propositionInfo.description, 'Prop desc');
+    assert.equal(propositionInfo.proposedBy, deployer);
+  });
+
+  it('it should fetch proposition pending status info', async () => {
+    const instance = await CommunityVoting.deployed();
+    const propositionStatus = await instance.getPropositionStatus(0);
+    assert.equal(propositionStatus.valueOf(), 0);
   });
 });
