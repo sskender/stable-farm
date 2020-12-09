c1d3635fc33b8064253900d542980fd91095bb83
sskender <sven.skender@fer.hr>
2020-12-09 15:16:53 +0100

    block info vue

---
+++ client/src/components/BlockInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrap-block-info">
+    Current block: <span>{{ this.blockNumber }}</span> <br />
+    Updated <span>{{ this.lastUpdateInSeconds }}</span> s ago.
+  </div>
+</template>
+
+<script>
+import web3 from "./../providers/web3";
+
+export default {
+  data: () => {
+    return { blockNumber: null, updatedAt: null, lastUpdateInSeconds: 0 };
+  },
+  methods: {
+    async updateBlockNumber() {
+      const block = await web3.eth.getBlockNumber();
+      this.blockNumber = block;
+      this.updatedAt = new Date();
+    },
+    async getTimeDifference() {
+      const timeDiff = new Date() - this.updatedAt;
+      this.lastUpdateInSeconds = Math.round(timeDiff / 1000);
+    },
+  },
+  async created() {
+    await this.updateBlockNumber();
+
+    // update block number
+    this.intervalUpdate = setInterval(
+      async () => await this.updateBlockNumber(),
+      10000
+    );
+
+    // update last updated time difference
+    this.intervalCalculate = setInterval(
+      async () => await this.getTimeDifference(),
+      1000
+    );
+  },
+};
+</script>
+
+<style scoped>
+</style>
+
--- client/src/components/DaoHome.vue
+++ client/src/components/DaoHome.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="dao-home">
     <div class="wrap-token">
+      <BlockInfo />
       <TokenInfo />
       <TokenHolders />
     </div>
@@ -9,12 +10,14 @@
 </template>
 
 <script>
+import BlockInfo from "./../components/BlockInfo.vue";
 import TokenInfo from "./../components/TokenInfo.vue";
 import TokenHolders from "./../components/TokenHolders.vue";
 
 export default {
   name: "DaoHome",
   components: {
+    BlockInfo,
     TokenInfo,
     TokenHolders,
   },
