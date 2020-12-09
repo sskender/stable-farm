d050d654e09095f6737b5c29d1b8313d6ce17dd3
sskender <sven.skender@fer.hr>
2020-12-09 14:15:13 +0100

    vue - token holders chairman added

--- client/src/components/TokenHolders.vue
+++ client/src/components/TokenHolders.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="wrap-token-holders">
-    <h3>Members</h3>
-    <p>Active members: {{ this.tokenHoldersList.length }}</p>
+    <h3>Active members</h3>
+    <p>
+      Chairman: <br />
+      <span id="chairman-address">{{ this.chairmanAddress }}</span>
+    </p>
+    <p>
+      Active members: <span>{{ this.tokenHoldersList.length }}</span>
+    </p>
     <ul>
       <li v-for="holder in tokenHoldersList" :key="holder">
-        {{ holder }}
+        <span id="holder-address"> {{ holder }} </span>
       </li>
     </ul>
   </div>
@@ -15,15 +21,20 @@ import DaoTokenContract from "./../providers/DaoTokenContract";
 
 export default {
   data: () => {
-    return { tokenHoldersList: [] };
+    return { tokenHoldersList: [], chairmanAddress: null };
   },
   methods: {
+    async getChairman() {
+      const chairman = await DaoTokenContract.methods.chairman().call();
+      this.chairmanAddress = chairman;
+    },
     async getTokenHolders() {
       const holders = await DaoTokenContract.methods.tokenHolders().call();
       this.tokenHoldersList = holders;
     },
   },
   async created() {
+    await this.getChairman();
     await this.getTokenHolders();
   },
 };
