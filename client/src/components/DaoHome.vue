d552ee0cf3c83ef548139fe5e504971dc9d5978e
sskender <sven.skender@fer.hr>
2020-12-10 15:48:01 +0100

    token mint vue

--- client/src/components/DaoHome.vue
+++ client/src/components/DaoHome.vue
@@ -3,6 +3,7 @@
     <div class="wrap-token">
       <BlockInfo />
       <TokenInfo />
+      <TokenMint />
       <TokenHolders />
     </div>
     <div class="wrap-vote"></div>
@@ -13,13 +14,15 @@
 import BlockInfo from "./../components/BlockInfo.vue";
 import TokenInfo from "./../components/TokenInfo.vue";
 import TokenHolders from "./../components/TokenHolders.vue";
+import TokenMint from "./../components/TokenMint.vue";
 
 export default {
   name: "DaoHome",
   components: {
     BlockInfo,
-    TokenInfo,
     TokenHolders,
+    TokenInfo,
+    TokenMint,
   },
 };
 </script>
---
+++ client/src/components/TokenMint.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="wrap-token-mint">
+    <div v-if="mintingEnabled">
+      <p>
+        We are accepting new members!<br />
+        <span>Claim your membership token here</span>
+      </p>
+      <button v-on:click="mintToken">JOIN</button>
+    </div>
+    <div v-else>We are not accepting new members at the moment</div>
+    <div v-if="chairmanConnected">
+      <span>Minting new tokens</span>
+      <button v-if="mintingEnabled" v-on:click="disableMinting">DISABLE</button>
+      <button v-else v-on:click="enableMinting">ENABLE</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import DaoTokenContract from "./../providers/DaoTokenContract";
+
+export default {
+  data: () => {
+    return {
+      chairmanAddress: null,
+      mintingEnabled: null,
+      chairmanConnected: true,
+    };
+  },
+  methods: {
+    async getChairman() {
+      const chairman = await DaoTokenContract.methods.chairman().call();
+      this.chairmanAddress = chairman;
+    },
+    async getMintingStatus() {
+      const minting = await DaoTokenContract.methods.isMintable().call();
+      this.mintingEnabled = minting;
+    },
+    async enableMinting() {
+      console.log("enabling minting");
+      this.mintingEnabled = true;
+    },
+    async disableMinting() {
+      console.log("disabling minting");
+      this.mintingEnabled = false;
+    },
+    async mintToken() {
+      console.log("minting started");
+    },
+  },
+  async created() {
+    await this.getChairman();
+    await this.getMintingStatus();
+  },
+};
+</script>
+
+<style scoped>
+</style>
+
