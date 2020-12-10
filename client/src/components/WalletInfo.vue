946d2dd4a542782b7a608677257f98193b2d9b6f
sskender <sven.skender@fer.hr>
2020-12-10 17:35:43 +0100

    wallet info metamask connect

--- client/src/components/WalletInfo.vue
+++ client/src/components/WalletInfo.vue
@@ -1,19 +1,51 @@
 <template>
   <div class="wrap-wallet-info">
-    <div v-if="walletAddress">
-      Hello, <span>{{ walletAddress }}</span>
+    <div v-if="account">
+      Hello, <span>{{ account }}</span> <br />
+      Balance: {{ accountBalance }}
     </div>
-    <div v-else>Please connect wallet</div>
+    <div v-else>
+      <span>Please connect wallet</span><br />
+      <button v-on:click="connectWallet">Connect</button>
+    </div>
   </div>
 </template>
 
 <script>
+import Web3 from "web3";
+import DaoTokenContract from "./../providers/DaoTokenContract";
+
 export default {
   data: () => {
     return {
-      walletAddress: "0x61d62800f58BE96883136b8d915a8E866d8a059b",
-      balance: 0,
+      account: null,
+      accountBalance: 0,
     };
+  },
+  methods: {
+    async loadWeb3() {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else {
+        window.alert(
+          "Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!"
+        );
+      }
+    },
+    async loadAccountData() {
+      const web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+      const balance = await DaoTokenContract.methods
+        .balanceOf(this.account)
+        .call();
+      this.accountBalance = balance;
+    },
+    async connectWallet() {
+      await this.loadWeb3();
+      await this.loadAccountData();
+    },
   },
 };
 </script>>
