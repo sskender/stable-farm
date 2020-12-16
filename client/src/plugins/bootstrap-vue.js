ce5cfff725a06121a290ef79870d7a8aab657c75
sskender <sven.skender@fer.hr>
2020-12-16 14:52:41 +0100

    icons support

--- client/src/plugins/bootstrap-vue.js
+++ client/src/plugins/bootstrap-vue.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
 
 import BootstrapVue from "bootstrap-vue";
+import BootstrapVueIcons from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue/dist/bootstrap-vue-icons.min.css";
+
+import { BIconArrowClockwise } from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+
+Vue.component("BIconArrowClockwise", BIconArrowClockwise);
