56b70227103d102998e894995944eb56e104f68b
sskender <sven.skender@fer.hr>
2020-12-20 15:47:58 +0100

    show number of total propositions

--- client/src/components/CommunityVoting/PropositionsList.vue
+++ client/src/components/CommunityVoting/PropositionsList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h4>All propositions</h4>
+    <div>
+      Total propositions: <span>{{ this.totalPropositions }}</span>
+    </div>
     <div v-for="proposition in propositionsList" :key="proposition.id">
       <PropositionItem :proposition="proposition" />
     </div>
@@ -12,17 +15,19 @@ import PropositionItem from "./PropositionItem.vue";
 
 export default {
   data: () => {
-    return { propositionsList: [] };
+    return { totalPropositions: 0, propositionsList: [] };
   },
   methods: {
     async loadPropositionsList() {
       const contract = this.$store.state.CommunityVotingContract;
 
+      // get total number of propositions
       const totalPropositions = await contract.methods
         .getNumberOfPropositions()
         .call();
+      this.totalPropositions = totalPropositions;
 
-      this.totalPropositions = [];
+      // load all propositions
       for (let i = 0; i < totalPropositions; i++) {
         const proposition = await contract.methods.getPropositionInfo(i).call();
         this.propositionsList.push(proposition);
