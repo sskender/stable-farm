e95581f84b30ee9c9e8d4f9dd0b8e00c341d686f
sskender <sven.skender@fer.hr>
2020-12-20 15:15:18 +0100

    load and show as text proposition status

--- client/src/components/CommunityVoting/PropositionItem.vue
+++ client/src/components/CommunityVoting/PropositionItem.vue
@@ -5,6 +5,7 @@
     <p>
       Proposed by <span>{{ proposition.proposedBy }}</span>
     </p>
+    <div>Status: {{ this.propositionStatusText }}</div>
     <div>
       Start: <span>{{ proposition.startBlock }}</span> End:
       <span>{{ proposition.endBlock }}</span>
@@ -15,10 +16,43 @@
 <script>
 export default {
   data: () => {
-    return {};
+    return { propositionStatus: 0, propositionStatusText: "Pending" };
   },
   props: {
     proposition: Object,
+  },
+  methods: {
+    showPropositionText(statusId) {
+      switch (statusId) {
+        case "0":
+          this.propositionStatusText = "Pending";
+          break;
+        case "1":
+          this.propositionStatusText = "Active";
+          break;
+        case "2":
+          this.propositionStatusText = "Succeeded";
+          break;
+        case "3":
+          this.propositionStatusText = "Failed";
+          break;
+        default:
+          break;
+      }
+    },
+    async getPropositionStatus() {
+      const contract = this.$store.state.CommunityVotingContract;
+      const status = await contract.methods
+        .getPropositionStatus(this.proposition.id - 1)
+        .call();
+      this.propositionStatus = status;
+
+      // display proposition status as text
+      this.showPropositionText(status);
+    },
+  },
+  async created() {
+    await this.getPropositionStatus();
   },
 };
 </script>
