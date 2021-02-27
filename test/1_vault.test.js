81556cbe84bb99f3a3a9362c381976122c83cc82
Sven Skender <sskender@pm.me>
2021-02-28 00:45:52 +0100

    vault - withdraw ether tests

--- test/1_vault.test.js
+++ test/1_vault.test.js
@@ -1,7 +1,7 @@
 const Vault = artifacts.require("Vault");
 
 contract("Vault", async (accounts) => {
-  it("it should deposit eth to vault", async () => {
+  it("it should deposit 5 eth to vault", async () => {
     const instance = await Vault.deployed();
     const sender = accounts[0];
     const amountToDeposit = "5";
@@ -19,5 +19,45 @@ contract("Vault", async (accounts) => {
     assert.isBelow(Number(balanceOfSenderAfter), Number(balanceOfSenderBefore));
     assert.equal(balanceBefore, "0");
     assert.equal(balanceAfter, amountToDeposit);
+  });
+
+  it("it should withdraw 1 eth from vault", async () => {
+    const instance = await Vault.deployed();
+    const withdrawer = accounts[0];
+    const amountToWithdraw = "1";
+
+    const balanceOfWithdrawerBefore = await web3.eth.getBalance(withdrawer);
+
+    const valueToWithdraw = web3.utils.toWei(amountToWithdraw, "ether");
+    await instance.withdrawEth(valueToWithdraw, { from: withdrawer });
+
+    const balanceOfWithdrawerAfter = await web3.eth.getBalance(withdrawer);
+    const balanceOfContractAfter = await instance.getBalance();
+    const amountWithdrawn = web3.utils.fromWei(balanceOfContractAfter, "ether");
+
+    assert.equal(amountWithdrawn, "4");
+    assert.isAbove(
+      Number(balanceOfWithdrawerAfter),
+      Number(balanceOfWithdrawerBefore)
+    );
+  });
+
+  it("it should withdraw all eth from vault (with overflow)", async () => {
+    const instance = await Vault.deployed();
+    const withdrawer = accounts[0];
+
+    const balanceOfWithdrawerBefore = await web3.eth.getBalance(withdrawer);
+
+    const valueToWithdraw = (await instance.getBalance()) + 10;
+    await instance.withdrawEth(valueToWithdraw, { from: withdrawer });
+
+    const balanceOfWithdrawerAfter = await web3.eth.getBalance(withdrawer);
+    const balanceOfContractAfter = await instance.getBalance();
+
+    assert.equal(balanceOfContractAfter, 0);
+    assert.isAbove(
+      Number(balanceOfWithdrawerAfter),
+      Number(balanceOfWithdrawerBefore)
+    );
   });
 });
