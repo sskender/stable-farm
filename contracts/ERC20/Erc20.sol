931dc1a74765d4d165f5383438d6493c0dfe80c5
Sven Skender <sskender@pm.me>
2021-03-21 18:39:26 +0100

    erc20 interface

--- contracts/ERC20/Erc20.sol
+++ contracts/ERC20/Erc20.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.6.0;
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
-interface ERC20 {
+interface Erc20 {
     /**
      * @dev Returns the amount of tokens in existence.
      */
