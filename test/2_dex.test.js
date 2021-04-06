d6021ee57f61ec1750afc9f061ffe8eadf3cd41c
Sven Skender <sskender@pm.me>
2021-04-06 20:16:56 +0200

    test dex - transfer comp tokens to uniswap contract

--- test/2_dex.test.js
+++ test/2_dex.test.js
@@ -3,8 +3,11 @@ const MainnetAddresses = require("./mainnet.addresses");
 const daiAbi = require("./abi/dai-abi.json");
 const erc20Abi = require("./abi/erc20-abi.json");
 
+const Erc20 = artifacts.require("Erc20");
+const Uniswap = artifacts.require("Uniswap");
+
 contract("Uniswap DEX", async (accounts) => {
-  it("it should mint 1000 test DAI tokens", async () => {
+  it("it should mint test DAI tokens", async () => {
     const daiMcdJoin = MainnetAddresses.DAI_MCD_JOIN;
     const daiAddress = MainnetAddresses.DAI_ADDRESS;
 
@@ -22,7 +25,7 @@ contract("Uniswap DEX", async (accounts) => {
     assert.isAtLeast(dai, 1000);
   });
 
-  it("it should mint 10 test COMP tokens", async () => {
+  it("it should mint test COMP tokens", async () => {
     const compAddress = MainnetAddresses.COMP_ADDRESS;
     const cCompAddress = MainnetAddresses.CCOMP_ADDRESS;
 
@@ -39,5 +42,37 @@ contract("Uniswap DEX", async (accounts) => {
     const comp = balance / 1e18;
 
     assert.isAtLeast(comp, 10);
+  });
+
+  it("it should transfer COMP tokens to contract", async () => {
+    const instance = await Uniswap.deployed();
+    const Comp = await Erc20.at(MainnetAddresses.COMP_ADDRESS);
+    const sender = accounts[0];
+    const tokensToTransfer = 2;
+
+    const valueToTransfer = web3.utils.toWei(
+      tokensToTransfer.toString(),
+      "ether"
+    );
+
+    const balanceUser = await Comp.balanceOf(sender);
+    const balanceContract = await Comp.balanceOf(instance.address);
+
+    await Comp.transfer(instance.address, valueToTransfer, { from: sender });
+
+    const balanceUserAfter = await Comp.balanceOf(sender);
+    const balanceContractAfter = await Comp.balanceOf(instance.address);
+
+    // test
+    assert.isAtLeast(Number(balanceUser), 10 * 1e18);
+    assert.equal(Number(balanceContract), 0);
+    assert.isAtLeast(Number(balanceUserAfter), 8 * 1e18);
+    assert.equal(Number(balanceContractAfter), 2 * 1e18);
+    assert.isBelow(Number(balanceUserAfter), Number(balanceUser));
+    assert.isAbove(Number(balanceContractAfter), Number(balanceContract));
+  });
+
+  it("it should swap COMP tokens for DAI tokens", async () => {
+    const instance = await Uniswap.deployed();
   });
 });
