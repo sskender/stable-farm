af87170b89e8d687a52da1c402908c2b3ac98fc5
Sven Skender <sskender@pm.me>
2021-04-07 22:52:53 +0200

    DAICompoundLeveragePool refactoring

--- contracts/pools/DAICompoundLeveragePool.sol
+++ contracts/pools/DAICompoundLeveragePool.sol
@@ -12,13 +12,16 @@ import "./../math/SafeMath.sol";
 import "./../math/SignedSafeMath.sol";
 
 contract DAICompoundLeveragePool is IPool, Uniswap {
-    uint256 internal constant MIN_COLLATERAL_VALUE = 25;
+    uint256 internal constant _MIN_UNDERLYING_VALUE = 25;
+    uint256 internal constant _UNDERLYING_DECIMALS = 18;
+
     address _uniswapRouterAddress;
+
     Comptroller private _comptroller;
     CErc20 private _cDai;
     Erc20 private _dai;
-    Erc20 private _rewardToken;
-    PriceFeed private _oracle;
+    Erc20 private _comp;
+    PriceFeed private _priceFeed;
 
     event Log(string, uint256);
 
@@ -33,9 +36,12 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
         _comptroller = Comptroller(_comptrollerAddress);
         _cDai = CErc20(_cDaiAddress);
         _dai = Erc20(_daiAddress);
-        _oracle = PriceFeed(_oracleAddress);
-        _rewardToken = Erc20(_comptroller.getCompAddress());
+        _priceFeed = PriceFeed(_oracleAddress);
         _uniswapRouterAddress = _uniswapAddress;
+
+        // Get COMP address
+        address _compAddress = _comptroller.getCompAddress();
+        _comp = Erc20(_compAddress);
 
         // Enter cDAI market
         _enterMarkets();
@@ -71,19 +77,22 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
 
     /// @dev Borrow asset from Compound protocol
     function _borrowAsset() private returns (uint256) {
-        // Get account's total liquidity
+        // Get contract total liquidity
         (uint256 liquidityError, uint256 liquidity, uint256 shortfall) =
             _comptroller.getAccountLiquidity(address(this));
-        if (liquidityError != 0) {
-            revert("Comptroller.getAccountLiquidity Error");
-        }
+
+        require(liquidityError == 0, "Comptroller.getAccountLiquidity Error");
         require(shortfall == 0, "account subjected to liquidation");
         require(liquidity > 0, "account has excess collateral");
 
         // Borrow maximum of underlying asset
-        uint256 cDaiPrice = _oracle.getUnderlyingPrice(address(_cDai));
+        uint256 cDaiPrice = _priceFeed.getUnderlyingPrice(address(_cDai));
         uint256 borrow =
-            SafeMath.mul(SafeMath.div(liquidity, cDaiPrice), 10**18);
+            SafeMath.mul(
+                SafeMath.div(liquidity, cDaiPrice),
+                10**_UNDERLYING_DECIMALS
+            );
+
         uint256 borrowError = _cDai.borrow(borrow);
         require(borrowError == 0, "CErc20.borrow Error");
 
@@ -95,11 +104,12 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
     /// @dev Supply the given amount of an asset to the protocol
     function deposit(uint256 _amount) external override {
         // Deposit token on sender's behalf
-        // This must be approved before calling the function
+        // User must approve before calling the function
         _dai.transferFrom(msg.sender, address(this), _amount);
 
-        // Supply and borrow
-        uint256 minimumCollateral = SafeMath.mul(MIN_COLLATERAL_VALUE, 10**18);
+        // Supply and borrow multiple times
+        uint256 minimumCollateral =
+            SafeMath.mul(_MIN_UNDERLYING_VALUE, 10**_UNDERLYING_DECIMALS);
         uint256 collateral = _amount;
         while (collateral > minimumCollateral) {
             _supplyCollateral(collateral);
@@ -127,11 +137,13 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
 
     /// @dev Claim COMP token rewards
     function harvest() external override {
-        uint256 rewardsBefore = _rewardToken.balanceOf(address(this));
-        _comptroller.claimComp(address(this));
-        uint256 rewardsAfter = _rewardToken.balanceOf(address(this));
+        uint256 compBefore = _comp.balanceOf(address(this));
 
-        uint256 harvested = SafeMath.sub(rewardsAfter, rewardsBefore);
+        _comptroller.claimComp(address(this));
+
+        uint256 compAfter = _comp.balanceOf(address(this));
+        uint256 harvested = SafeMath.sub(compAfter, compBefore);
+
         emit Harvest(harvested);
     }
 
@@ -140,11 +152,10 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
 
     /// @dev Withdraw all supplied collateral and rewards
     function withdrawAll() external override {
-        // Get total borrow amount
-        // This must be approved before calling the function
+        // Transfer more DAI to repay borrow if necessary
+        // User must approve before calling the function
         uint256 borrow = _cDai.borrowBalanceCurrent(address(this));
 
-        // Transfer more DAI to repay borrow if necessary
         uint256 availableForRepay = _dai.balanceOf(address(this));
         int256 shortage =
             SignedSafeMath.sub(int256(borrow), int256(availableForRepay));
@@ -155,22 +166,17 @@ contract DAICompoundLeveragePool is IPool, Uniswap {
         // Close leverage position
         _closePosition(borrow);
 
-        // Claim COMP token
+        // Claim COMP token rewards
         this.harvest();
+        uint256 amountIn = _comp.balanceOf(address(this));
+        this.swapTokensAForTokensB(
+            address(_comp),
+            address(_dai),
+            _uniswapRouterAddress,
+            amountIn
+        );
 
-        uint256 amountIn = _rewardToken.balanceOf(address(this));
-        emit Log("comp amount:", amountIn);
-
-        uint256 amountDaiBefore = _dai.balanceOf(address(this));
-        emit Log("dai before:", amountDaiBefore);
-
-        // swap COMP for DAI
-        this.swapTokensAForTokensB(address(_rewardToken), address(_dai),_uniswapRouterAddress,amountIn);
-
-        uint256 amountDaiAfter= _dai.balanceOf(address(this));
-        emit Log("dai after:", amountDaiAfter);
-
-        // Transfer reclaimed DAI + rewards converted to DAI back to user
+        // Transfer reclaimed DAI + rewards back to user
         uint256 balance = _dai.balanceOf(address(this));
         _dai.transfer(msg.sender, balance);
 
