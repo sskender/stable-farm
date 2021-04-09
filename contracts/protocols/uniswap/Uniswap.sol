35e303b1aac5e42d035ad12aedc031f2f508fe8a
Sven Skender <sskender@pm.me>
2021-04-10 00:36:36 +0200

    Uniswap refactoring magic numbers

--- contracts/protocols/uniswap/Uniswap.sol
+++ contracts/protocols/uniswap/Uniswap.sol
@@ -7,6 +7,9 @@ import "./IUniswapV2Router02.sol";
 import "./../../math/SafeMath.sol";
 
 contract Uniswap {
+    uint256 internal constant _MIN_LEFT_AFTER_SLIPPAGE = 98;
+    uint256 internal constant _DEADLINE_IN_SECONDS = 15;
+
     /// @dev Swap exact number of tokens A for as many tokens B
     function swapTokensAForTokensB(
         address _tokenA,
@@ -26,10 +29,12 @@ contract Uniswap {
         path[2] = address(tokenB);
 
         uint256[] memory amounts = uniswapRouter.getAmountsOut(_amountIn, path);
+        // amountOutMin = amountOutMin * 0.98
         uint256 amountOutMin =
-            SafeMath.div(SafeMath.mul(amounts[amounts.length - 1], 98), 100);
+            SafeMath.div(SafeMath.mul(amounts[amounts.length - 1], _MIN_LEFT_AFTER_SLIPPAGE), 100);
 
-        uint256 deadline = SafeMath.add(block.timestamp, 15);
+        // time.now() + 15 sec
+        uint256 deadline = SafeMath.add(block.timestamp, _DEADLINE_IN_SECONDS);
 
         uniswapRouter.swapExactTokensForTokens(
             _amountIn,
