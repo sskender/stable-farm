50dd85301d9f33aade1a78acd0b80ba999b47562
Sven Skender <sskender@pm.me>
2021-04-12 14:52:36 +0200

    Uniswap - added constructor

--- contracts/controllers/Uniswap.sol
+++ contracts/controllers/Uniswap.sol
@@ -2,41 +2,53 @@
 
 pragma solidity ^0.6.0;
 
-import "./../../ERC20/Erc20.sol";
-import "./IUniswapV2Router02.sol";
-import "./../../math/SafeMath.sol";
+import "./../ERC20/Erc20.sol";
+import "./../protocols/uniswap/IUniswapV2Router02.sol";
+import "./../math/SafeMath.sol";
 
 contract Uniswap {
     uint256 internal constant _MIN_LEFT_AFTER_SLIPPAGE = 98;
     uint256 internal constant _DEADLINE_IN_SECONDS = 15;
 
+    IUniswapV2Router02 private _uniswapRouter;
+
+    constructor(address _uniswapRouterAddress) public {
+        _uniswapRouter = IUniswapV2Router02(_uniswapRouterAddress);
+    }
+
     /// @dev Swap exact number of tokens A for as many tokens B
-    function swapTokensAForTokensB(
+    function _swapTokensAForTokensB(
         address _tokenA,
         address _tokenB,
-        address _uniswapRouter,
         uint256 _amountIn
-    ) external {
+    ) internal {
         Erc20 tokenA = Erc20(_tokenA);
         Erc20 tokenB = Erc20(_tokenB);
-        IUniswapV2Router02 uniswapRouter = IUniswapV2Router02(_uniswapRouter);
 
-        tokenA.approve(address(uniswapRouter), _amountIn);
+        tokenA.approve(address(_uniswapRouter), _amountIn);
 
         address[] memory path = new address[](3);
         path[0] = address(tokenA);
-        path[1] = uniswapRouter.WETH();
+        path[1] = _uniswapRouter.WETH();
         path[2] = address(tokenB);
 
-        uint256[] memory amounts = uniswapRouter.getAmountsOut(_amountIn, path);
+        uint256[] memory amounts =
+            _uniswapRouter.getAmountsOut(_amountIn, path);
+
         // amountOutMin = amountOutMin * 0.98
         uint256 amountOutMin =
-            SafeMath.div(SafeMath.mul(amounts[amounts.length - 1], _MIN_LEFT_AFTER_SLIPPAGE), 100);
+            SafeMath.div(
+                SafeMath.mul(
+                    amounts[amounts.length - 1],
+                    _MIN_LEFT_AFTER_SLIPPAGE
+                ),
+                100
+            );
 
         // time.now() + 15 sec
         uint256 deadline = SafeMath.add(block.timestamp, _DEADLINE_IN_SECONDS);
 
-        uniswapRouter.swapExactTokensForTokens(
+        _uniswapRouter.swapExactTokensForTokens(
             _amountIn,
             amountOutMin,
             path,
