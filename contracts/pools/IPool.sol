9bcdf5856b4e86f07778e17f6c64059e87e7c1b4
Sven Skender <sskender@pm.me>
2021-04-14 16:45:35 +0200

    IPool interface docs updated

--- contracts/pools/IPool.sol
+++ contracts/pools/IPool.sol
@@ -12,33 +12,33 @@ interface IPool {
     /// @dev Event upon successful rebalance
     event Rebalanced(uint256);
 
-    /// @dev
+    /// @dev Add new router address to the pool protocol
     function addRouter(address _newRouter) external;
 
-    /// @dev
+    /// @dev Get all available routers in the pool protocol
     function getRouters() external view returns (address[] memory);
 
-    /// @dev
+    /// @dev Get the Ethereum address of the underlying asset
     function getAssetAddress() external view returns (address);
 
-    /// @dev
+    /// @dev Get the name of the underlying asset
     function getAssetName() external view returns (string memory);
 
-    /// @dev
+    /// @dev Get the symbol of the underlying asset
     function getAssetSymbol() external view returns (string memory);
 
-    /// @dev
+    /// @dev Get the current APY for farming asset
     function getCurrentAPY() external view returns (uint256);
 
-    /// @dev
+    /// @dev Get the best available APY
     function getBestAPY() external view returns (uint256);
 
-    /// @dev
+    /// @dev Deposit underlying asset to the pool protocol
     function deposit(uint256 _amount) external;
 
-    /// @dev
+    /// @dev Withdraw underlying asset from the pool protocol
     function withdraw(uint256 _amount) external;
 
-    /// @dev
+    /// @dev Rebalance to the best available APY
     function rebalance() external;
 }
